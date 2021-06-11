2b748f555b662e4881b1e2556405cafba17d615d
Donna <donnastjames@comcast.net>
2021-06-10 22:39:09 -0700

    Progress on employeeDBSchema.sql and employeeTracker.js

--- employeeDBSchema.sql
+++ employeeDBSchema.sql
@@ -4,22 +4,51 @@ CREATE DATABASE employeeDB;
 USE employeeDB;
 
 CREATE TABLE department(
-  id INT PRIMARY KEY (id) NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30),
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE role(
-  id INT PRIMARY KEY (id) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `role`(
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30),
-  salary DECIMAL (6,2),
+  salary DECIMAL (10,2),
   department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY fk_department (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
-  id INT PRIMARY KEY (id) NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL, 
   role_id INT NOT NULL,
   manager_id INT,
-  
+  PRIMARY KEY (id),
+  FOREIGN KEY fk_role (role_id) REFERENCES `role`(id),
+  FOREIGN KEY fk_manager (manager_id) REFERENCES employee(id)
 );
+
+INSERT INTO department (name) VALUE ("Executive");
+
+INSERT INTO `role` (title, salary, department_id) VALUES ("CEO", 80000, 1);
+
+INSERT INTO `role` (title, salary, department_id) VALUES ("Manager", 60000, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("John", "Smith", 1, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Sally", "Taylor", 2, 2);
+
+USE employeeDB;
+
+SELECT 
+  employee.*, 
+  employeeManager.first_name AS manager_first, 
+  employeeManager.last_name AS manager_last,
+  `role`.title AS role_title,
+  `role`.salary AS role_salary,
+  department.name AS department_name
+FROM employee
+INNER JOIN employee AS employeeManager ON employee.manager_id = employeeManager.id
+INNER JOIN `role` ON employee.role_id = `role`.id
+INNER JOIN department ON `role`.department_id = department.id
--- employeeTracker.js
+++ employeeTracker.js
@@ -1,16 +1,133 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const Employee = require('./lib/employee');
+const Role = require('./lib/role');
+const Department = require('./lib/department');
 
 const connection = mysql.createConnection({
   host: 'localhost',
 
-  // Your port; if not 3306
   port: 3306,
 
-  // Your username
   user: 'root',
 
-  // Your password
   password: 'UrPr3qu3l',
   database: 'employeeDB',
 });
+
+connection.connect((err) => {
+  if (err) throw err;
+  runQuery();
+});
+
+const runQuery = () => {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'rawlist',
+      message: 'What would you like to do?',
+      choices: [
+        'View All Employees',
+        'View All Employees By Department',
+        'View All Employees By Manager',
+        'Add Employee',
+        'Update Employee Role',
+        'View All Roles',
+        'View All Departments',
+        'Exit',
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case 'Add Employee':
+          addEmployee();
+          break;
+        case 'Exit':
+          // TODO: Something that exits the app.
+      }
+    });
+};
+
+const managerSearch = () => {
+  const query = 
+    'SELECT * FROM employee WHERE manager_id IS NOT NULL';
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    res(employee) => {console.log }
+    
+  });
+}
+
+const addEmployee = () => {
+  inquirer
+    .prompt(
+      {
+      name: 'first_name',
+      type: 'input',
+      message: `What is the employee's first name?`,
+      },
+      {
+        name: 'last_name',
+        type: 'input',
+        message: `What is the employee's last name?`,
+      },
+      {
+        name: 'role_id',
+        type: 'rawlist',
+        message: `What is the employee's role?`,
+        choices: [
+          'Sales Lead',
+          'Sales Person',
+          'Lead Engineer',
+          'Software Engineer',
+          'Account Manager',
+          'Accountant',
+          'Legal Team Lead',
+          'Lawyer',
+        ],
+      },
+      {
+        name: 'salary',
+        type: 'input',
+        message: `What is the employee's salary?`,
+      },
+    ).then((answers) => {
+      const choices = 'SELECT first_name, last_name, role_id FROM employee FULL JOIN role on employee.id = role.id';
+      connection.choices(choices, [answer.first_name, answer.last_name, answer.role_id, answer.title, answer.salary], (err,res) => {
+        if (err) throw err;
+        res.forEach(({ 
+
+        }))
+      } )
+    });
+}
+
+const getManagers = () => {
+  const choices = employeeDB.managerSearch();
+  console.log(choices);
+  return managerSearch((err,res) => {
+    inquirer
+      .prompt(
+        {
+        name: 'manager_id',
+        type: 'rawlist',
+        message: `Please select the employee's manager from the list: `,
+        choices: choices,
+        }
+      ).then(({manager_id}) => {
+        console.table(manager_id);
+        res();
+      });
+      
+      getManagers();
+  });
+}
+    // .then(answer => {
+    //   const insert = `INSERT INTO employee VALUES ("${answer.firstName}", "${answer}")`;
+    //   connection.query(insert, (err, res) => {
+    //     if (err) throw err;
+
+    //   })
+        
+      
+    
